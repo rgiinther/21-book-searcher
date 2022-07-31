54c7d123e1e125d9225fd51436e794671b8b9d90
rgiinther <r.giin95@gmail.com>
2022-07-31 11:00:30 -0500

    added queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+import { gql } from '@apollo/client';
+
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}`;
+
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user{
+            _id
+            username
+        }
+    }
+}`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookInfo: bookData!) {
+      saveBook(bookInfo: $bookData) {
+        token { 
+        bookData{ 
+            description
+            title
+            bookId
+            image
+            link
+            authors
+         }
+      }  
+    }`;
+
+    export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $String) {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+    `;
+
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 type Book {
     bookId: String
-    authors: [Strings]
+    authors: [String]
     description: String
     title: String
     image: String
@@ -17,26 +17,31 @@ type User {
     bookCount: Int
     savedBook: [Book]
 }
-type Query {
-    me: User
-}
+
 input bookData {
     description: String
     title: String
     bookId: String
     image: String
     link: String
-    authors: [Strings]
+    authors: [String]
 }
+
+type Auth {
+    token: String
+    user: User
+}
+
+type Query {
+    me: User
+}
+
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(bookInfo: bookData) : User
     removeBook(bookId: String): User
 }
-type Auth {
-    token: String
-    user: User
-}`;
+`;
 
 module.exports = typeDefs;
